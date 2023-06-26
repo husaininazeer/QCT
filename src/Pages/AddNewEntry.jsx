769ce2eb336f844bfc272c9cd5f314181ea1e3ed
Mohamed <husaini.nazeer@gmail.com>
2023-06-26 23:57:50 +0100

    WIP: render form for new values page

--- src/Pages/AddNewEntry.jsx
+++ src/Pages/AddNewEntry.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-// import
+import { Heading, Stack, Center, Button, Select, Input } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
 function AddNewEntry() {
-  return <div>AddNewEntry</div>
+  const navigate = useNavigate()
+
+  const handleBackPress = () => {
+    navigate(-1)
+  }
+
+  return (
+    <>
+      <Heading align="center" color="#012169" fontSize="75px">
+        QurCan Transcriber
+      </Heading>
+      <Button onClick={handleBackPress}> Back to main table </Button>
+      <Stack direction="column" shouldWrapChildren>
+        <Center>
+          <Input placeholder="Mutation Type"></Input>
+          <Input placeholder="notes"></Input>
+        </Center>
+        <Button>Add new entry</Button>
+      </Stack>
+    </>
+  )
 }
 
 export default AddNewEntry
